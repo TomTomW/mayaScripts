2e7efd7b1515235388ace43c226aabe3c152fa5d
Tom Whitzer <tjwhitzer@gmail.com>
2017-11-20 15:18:47 -0500

    no message

--- jointCreator.py
+++ jointCreator.py
@@ -2,12 +2,10 @@ from pymel.core import *
 
 # Takes in a list of selected objs in the order the joints will be created. 
 
-def createJoints(selection = selected()):
-	select(clear=True)
-
-	for i in selection:
-		joint()
-		newJoint = ls(sl=1, o=1)
-		matchTransform(newJoint, i)
+def createJoints(selection=selected()):
+    for item in selection:
+        matchTransform(joint(), item)
+    #parent = selected()[0].root()
+	#orientJoint(parent)
 
 createJoints()
