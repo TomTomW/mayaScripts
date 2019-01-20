c5e5b1f9c1e0f973065bbf5eb83e57fd3a72f288
Tom Whitzer <tjwhitzer@gmail.com>
2019-01-19 18:27:20 -0600

    Took out all of the Pymel. Made more efficient along with being able to be run on multiple selected objects. Also you can change the name that is being tagged onto the newly created group

--- controlGroup.py
+++ controlGroup.py
@@ -1,27 +1,34 @@
 import maya.cmds as cmds
-import pymel.core as pm
 
 ''' create group with the name of the selected object with ctrlSpace at the end.
     Match transforms of the selected object and parent selected object under new group'''
 
 
-selection = pm.ls(sl=True)
-selectionName = selection[0]
+# -------------------------------------------------------------------------
+def ctrlSpace(targets, name='Space'):
+    # Parents target under a group created at the same location.
 
-newGroup = pm.group(em=True, name=selectionName + 'Space')
+    try:
+        if len(targets) > 1:
+            for target in targets:
+                ctrlGrp = cmds.group(em=True, name=target + name)
+                matchXfos(target, ctrlGrp)
+                cmds.parent(target, ctrlGrp)
 
-def matchXfos():
+        else:
+            ctrlGrp = cmds.group(em=True, name=targets[0] + name)
+            matchXfos(targets[0], ctrlGrp)
+            cmds.parent(targets[0], ctrlGrp)
 
-    # get selection
-    
-    srcObj = selection[0]
-    targets = newGroup
-    
-        
-    srcXfo = pm.xform(srcObj, query=True, worldSpace=True, matrix=True)
-      
-    pm.xform(targets, worldSpace=True, matrix=srcXfo)
+    except IndexError:
+        print '//Error//: Something needs to be selected for ctrlSpace() to run!!!'
 
-matchXfos()
+# -------------------------------------------------------------------------
+def matchXfos(srcObj, target):
+    # match transforms of srcObj to the target
 
-pm.parent(selectionName, newGroup)
+    srcXfo = cmds.xform(srcObj, query=True, worldSpace=True, matrix=True)
+    cmds.xform(target, worldSpace=True, matrix=srcXfo)
+
+
+ctrlSpace(cmds.ls(sl=1), 'Space')
